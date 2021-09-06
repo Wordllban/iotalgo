5deeff59dced91687080890834598a8daec499d6
KMininkov <mininkov3228@gmail.com>
2021-09-07 01:11:23 +0300

    added HeapSort algorithm

---
+++ app.js
@@ -0,0 +1,43 @@
+function heapify(arr, length, i) {
+    let largest = i
+    let left = i * 2 + 1
+    let right = left + 1
+
+    if (left < length) {
+        if(arr[left] > arr[largest]) {
+            largest = left
+        }
+    }
+
+    if (right < length) {
+        if(arr[right] > arr[largest]) {
+            largest = right
+        }
+    }
+
+    if(largest != i) {
+        [arr[i], arr[largest]] = [arr[largest], arr[i]]
+        heapify(arr, length, i)
+    }
+    return arr
+}
+
+function heapSort(arr) {
+    let length = arr.length
+    let i = Math.floor(length / 2 - 1)
+    let k = length - 1
+    
+    while (i >= 0) {
+        heapify(arr, length, i)
+        i--
+    }
+
+    while (k >= 0) {
+        [arr[0], arr[k]] = [arr[k], arr[0]]
+        heapify(arr, k, 0)
+        k--
+    }
+
+    return arr
+}
+
