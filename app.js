0f9972c3a1ef81302e0c3aa03a1ed8832216af58
KMininkov <mininkov3228@gmail.com>
2021-09-07 22:00:27 +0300

    Fixed heapify func, added comparison/swap counters

--- app.js
+++ app.js
@@ -1,43 +1,62 @@
-function heapify(arr, length, i) {
-    let largest = i
-    let left = i * 2 + 1
-    let right = left + 1
+function heapify(arr, length, i, counter = { comparisons: 0, swaps: 0, executionTime: 0 }) {
+    let largest = i;
+    let left = i * 2 + 1;
+    let right = left + 1;
 
+
+    counter.comparisons++;
     if (left < length) {
+        counter.comparisons++; //for next 'if'
         if(arr[left] > arr[largest]) {
-            largest = left
+            largest = left;
         }
     }
 
+    counter.comparisons++;
     if (right < length) {
+        counter.comparisons++; //for next 'if'
         if(arr[right] > arr[largest]) {
-            largest = right
+            
+            largest = right;
         }
     }
 
     if(largest != i) {
-        [arr[i], arr[largest]] = [arr[largest], arr[i]]
-        heapify(arr, length, i)
+        counter.swaps++;
+        [arr[i], arr[largest]] = [arr[largest], arr[i]];
+        heapify(arr, length, largest, counter);
     }
-    return arr
 }
 
 function heapSort(arr) {
-    let length = arr.length
-    let i = Math.floor(length / 2 - 1)
-    let k = length - 1
+    const startTime = performance.now();
+    let length = arr.length;
+    let i = Math.floor(length / 2 - 1);
+    let k = length - 1;
     
+    let counter = {
+        comparisons: 0, 
+        swaps: 0 
+    }
     while (i >= 0) {
-        heapify(arr, length, i)
-        i--
+        heapify(arr, length, i, counter);
+        i--;
     }
 
     while (k >= 0) {
-        [arr[0], arr[k]] = [arr[k], arr[0]]
-        heapify(arr, k, 0)
-        k--
+        counter.swaps++;
+        [arr[0], arr[k]] = [arr[k], arr[0]];
+        heapify(arr, k, 0, counter);
+        k--;
     }
-
-    return arr
+    
+    const endTime = performance.now();
+    counter.executionTime = endTime - startTime
+    console.log("HeapSort")
+    console.log("Sorted Array:", ...arr);
+    
+    return arr, counter;
 }
-
+let arr = [4, 2, 9, 7, 1, 3, 8, 0, 5, 6];
+let counter = heapSort(arr);
+console.log("counters:", counter);
