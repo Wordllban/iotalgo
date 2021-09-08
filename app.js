b10b65678d878bd7d4f9b70b3d8d7388bfdfda14
KMininkov <mininkov3228@gmail.com>
2021-09-08 15:49:08 +0300

    added asc or desc sort order

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function heapify(arr, length, i, counter = { comparisons: 0, swaps: 0, executionTime: 0 }) {
+function heapifyMax(arr, length, i, counter = { comparisons: 0, swaps: 0, executionTime: 0 }) {
     let largest = i;
     let left = i * 2 + 1;
     let right = left + 1;
@@ -24,11 +24,41 @@ function heapify(arr, length, i, counter = { comparisons: 0, swaps: 0, execution
     if(largest != i) {
         counter.swaps++;
         [arr[i], arr[largest]] = [arr[largest], arr[i]];
-        heapify(arr, length, largest, counter);
+        heapifyMax(arr, length, largest, counter);
     }
 }
 
-function heapSort(arr) {
+function heapifyMin(arr, length, i, counter = { comparisons: 0, swaps: 0, executionTime: 0 }) {
+    let largest = i;
+    let left = i * 2 + 1;
+    let right = left + 1;
+
+
+    counter.comparisons++;
+    if (left < length) {
+        counter.comparisons++; //for next 'if'
+        if(arr[left] < arr[largest]) {
+            largest = left;
+        }
+    }
+
+    counter.comparisons++;
+    if (right < length) {
+        counter.comparisons++; //for next 'if'
+        if(arr[right] < arr[largest]) {
+            
+            largest = right;
+        }
+    }
+
+    if(largest != i) {
+        counter.swaps++;
+        [arr[i], arr[largest]] = [arr[largest], arr[i]];
+        heapifyMin(arr, length, largest, counter);
+    }
+}
+
+function heapSort(arr, sortOrder = "asc") {
     const startTime = performance.now();
     let length = arr.length;
     let i = Math.floor(length / 2 - 1);
@@ -38,17 +68,34 @@ function heapSort(arr) {
         comparisons: 0, 
         swaps: 0 
     }
-    while (i >= 0) {
-        heapify(arr, length, i, counter);
-        i--;
+    if(sortOrder === "asc" ) {
+        while (i >= 0) {
+            heapifyMax(arr, length, i, counter);
+            i--;
+        }
+    
+        while (k >= 0) {
+            counter.swaps++;
+            [arr[0], arr[k]] = [arr[k], arr[0]];
+            heapifyMax(arr, k, 0, counter);
+            k--;
+        }
+    } else if (sortOrder === "desc") {
+        while (i >= 0) {
+            heapifyMin(arr, length, i, counter);
+            i--;
+        }
+    
+        while (k >= 0) {
+            counter.swaps++;
+            [arr[0], arr[k]] = [arr[k], arr[0]];
+            heapifyMin(arr, k, 0, counter);
+            k--;
+        }
+    } else {
+        console.log("Wrong sort order")
     }
-
-    while (k >= 0) {
-        counter.swaps++;
-        [arr[0], arr[k]] = [arr[k], arr[0]];
-        heapify(arr, k, 0, counter);
-        k--;
-    }
+    
     
     const endTime = performance.now();
     counter.executionTime = endTime - startTime
