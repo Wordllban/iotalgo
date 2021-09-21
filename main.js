3169ebc0dfa349a1dd1ece17ee86dd266bd2525b
KMininkov <mininkov3228@gmail.com>
2021-09-21 23:31:30 +0300

    Added doubly linked list push, pop, unshift methods

---
+++ main.js
@@ -0,0 +1,76 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.prev = null;
+        this.next = null; 
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    // add element to the end
+    push(value) {
+        let node = new Node(value)
+        // if list is empty 
+        if(!this.head) {
+            this.head = node;
+            this.tail = node;
+        } else { 
+            let temp = this.tail;
+            this.tail = node;
+            node.prev = temp;
+            temp.next = node;
+        }
+        this.length++;
+        return this;
+    } 
+
+    // delete last element and return his value
+    pop() {
+        console.log(this.tail);
+        if(!this.head) return undefined;
+        let temp = this.tail;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = temp.prev;
+            this.tail.next = null;
+            temp.prev = null;
+        }
+        this.length--;
+        return list;
+    }
+    
+    // add element in front
+    unshift(value) {
+        let node = new Node(value);
+        if(!this.head) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            let temp = this.head;
+            this.head = node;
+            node.next = temp;
+            temp.prev = node;
+        }
+        this.length++;
+        return this;
+    }
+
+}
+
+
+let list = new DoublyLinkedList;
+/* list.push(1);
+list.push(2);
+list.push(3);
+list.push(4); */
+//list.pop();
+list.unshift(0);
+console.log(list);
